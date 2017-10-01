cdcc78a5d2b5962df3ba82ee7aa208aef3cadafd
ardenn <ardenn@users.noreply.github.com>
2017-10-01 19:56:40 +0000

    Added comments to explaining the code sections

--- account.py
+++ account.py
@@ -4,8 +4,11 @@ import bcrypt
 
 
 class Account:
+    # Account constructor method - instantiates a new Account object
 
     def __init__(self, firstname, lastname, gender, username, password):
+
+        # reads accounts.csv to check if username is available
         with open("accounts.csv", "r") as file_handler:
             reader = csv.DictReader(file_handler)
             user_available = True
@@ -16,6 +19,8 @@ class Account:
                         raise ValueError
                     except ValueError:
                         print("Sorry, that username is taken!")
+
+            # if username is available
             if user_available:
                 hashed = bcrypt.hashpw(
                     password.encode("utf-8"), bcrypt.gensalt())
@@ -25,6 +30,7 @@ class Account:
                 self.username = username
                 self.password = hashed
 
+                # writes the new object to accounts.csv
                 with open("accounts.csv", "a") as file_handler:
                     fieldnames = [
                         "firstname",
