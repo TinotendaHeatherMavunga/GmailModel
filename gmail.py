5d4bc761350371bc88c65524bf98d81a82981a1f
Samuel Sowah <samuel@official-notebook.com>
2017-10-02 10:31:41 +0000

    code cleanup

--- gmail.py
+++ gmail.py
@@ -6,36 +6,31 @@ import datetime
 class Gmail:
 
     def __init__(self):
+        self.user_inbox = []
         pass
 
     def send_email(self, to_email, msg, subject, user):
-
-        from_address = user.email     # 'gtestigwe@gmail.com'
         to_address = to_email
-
         subject = subject
-        msg  = 'Subject: {}\n\n{}'.format(subject, msg)
-
-        password = user.password #password goes here
+        msg = 'Subject: {}\n\n{}'.format(subject, msg)
 
         try:
             with open('messages.csv', 'a') as file:
-                messages= csv.DictWriter(file, fieldnames=['from', 'to', 'message', 'timestamp'])
-                messages.writerow({'from': from_address, 'to': to_address, 'message': msg, 'timestamp': datetime.now() })
+                messages = csv.DictWriter(file, fieldnames=['from', 'to', 'message', 'timestamp'])
+                messages.writerow({'from': user.email, 'to': to_address, 'message': msg, 'timestamp': datetime.datetime.now() })
 
                 print('Message sent successfully to {}'.format(to_email))
 
         except Exception as e:
             print('Sorry email was not sent, Try again', e)
 
-    def compose_email(self, message='', to='', subject=''):
+    def compose_email(self, user):
         subject = input('Please enter subject: ')
         message = input("Compose new message here: ")
         to = input('Enter email address: ')
-        self.send_email(to, message, subject)
+        self.send_email(to, message, subject, user)
 
     def inbox(self, user):
-        self.user_inbox = []
         if user.logged_in():
             with open('messages.csv', 'r') as file:
                 messages = csv.DictReader(file, fieldnames=['message', 'from', 'to', 'timestamp'])
