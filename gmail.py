dd66cd31671125e52b77eac8903c1c0b7a7e4dd9
Samuel Sowah <samuel@official-notebook.com>
2017-10-02 10:18:45 +0000

    created inbox function

--- gmail.py
+++ gmail.py
@@ -1,2 +1,21 @@
+import csv
+
+
 class Gmail:
-    pass
+
+    def __init__(self):
+        pass
+
+    def inbox(self, user):
+        user_inbox = []
+        if user.logged_in():
+            with open('messages.csv', 'r') as file:
+                messages = csv.DictReader(file, fieldnames=['message', 'from', 'to', 'timestamp'])
+                for message in messages:
+                    if message['to'] == user['email']:
+                        user_inbox.append(message)
+
+            return user_inbox
+
+        else:
+            raise Exception('{} is not logged in.'.format(user.username))
